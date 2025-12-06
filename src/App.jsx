d9b3c3f7329ef45e705b9f3141c554eb36d3c07a
miro web <mirodiljs2004@gmail.com>
2025-12-06 15:50:21 +0500

    react add

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
